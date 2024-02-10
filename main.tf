e051fb3af4cc8b658ec4822fbe5f83e0f4fec1ce
ccluskey <cjc12358@hotmail.com>
2024-02-09 16:38:02 -0800

    I have no idea if this will work

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.10.101.0/20", "10.10.102.0/20", "10.10.103.0/20"]
 
   tags = {
     Terraform = "true"
