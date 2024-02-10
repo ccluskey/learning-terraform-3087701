0c750ebfbabbd0d9d3f54a40568ea1066408f26f
ccluskey <cjc12358@hotmail.com>
2024-02-09 16:13:50 -0800

    Add autoscalling

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_vpc" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -67,16 +67,21 @@ module "alb" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  image_id       = data.aws_ami.app_ami.id
+  instance_type  = var.instance_type
 
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "blog_sg" {
